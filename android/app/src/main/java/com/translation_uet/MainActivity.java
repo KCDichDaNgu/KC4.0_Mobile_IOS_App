2e5fd94eef01511fc51a764171cfb75637d2284e
KhaiNguyen <khaidnguyen12345@gmail.com>
2021-10-27 10:04:14 +0700

    Initial commit

---
+++ android/app/src/main/java/com/translation_uet/MainActivity.java
@@ -0,0 +1,15 @@
+package com.translation_uet;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "translation_uet";
+  }
+}
